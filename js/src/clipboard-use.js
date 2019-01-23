5f2cc59485276344e228863b8788eb81a21bc788
huangguangda <2397923107@qq.com>
2019-01-23 12:14:36 +0800

    Site updated: 2019-01-23 12:14:28

---
+++ js/src/clipboard-use.js
@@ -0,0 +1,1 @@
+!function(n,t,o){var a=function(){var n="";n+='<button class="btn-copy" data-clipboard-snippet="">';n+='  <i class="fa fa-globe"></i><span>copy</span>';n+="</button>";$(".highlight .code pre").before(n);new ClipboardJS(".btn-copy",{target:function(n){return n.nextElementSibling}})};a()}(window,document);
